d07ecdbc0bcb33c68991cc39b13925f8e5382c83
chenqiu1024 <godspeed1024@gmail.com>
2018-05-12 18:17:36 +0800

    S.I.W.

---
+++ cleanup.sh
@@ -0,0 +1,9 @@
+rm -f *.cer
+rm -f *.crt
+rm -f *.csr
+rm -f *.pem
+rm -f *.der
+rm -f *.key
+rm -f *.p12
+rm -rf demoCA
+
--- createCustomCertReq.sh
+++ createCustomCertReq.sh
@@ -1,5 +1,11 @@
 ## http://rhythm-zju.blog.163.com/blog/static/310042008015115718637/
 ## http://www.360doc.com/content/15/1224/18/18635950_522843628.shtml
-openssl genrsa -des3 -out server_key.key
+openssl genrsa -des3 -out server_key.key.orig
+openssl rsa -in server_key.key.orig -out server_key.key
+rm -f server_key.key.orig
 openssl req -new -days 3650 -key server_key.key -out server_req.csr
+
+## ???
+##openssl x509 -req -in server_req.csr -out server_cert.pem -signkey server_key.key -CA demoCA/cacert.pem -CAkey demoCA/private/cakey.pem -CAcreateserial -days 3650
+
 
