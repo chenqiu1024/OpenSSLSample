0283f405f2ad4edc10046f42c431599f2f1ff5fb
chenqiu1024 <godspeed1024@gmail.com>
2018-05-22 19:17:11 +0800

    Expire days 3650;

--- allInOne.sh
+++ allInOne.sh
@@ -1,4 +1,10 @@
 rm -rf demoCA
+rm -f *.der
+rm -f *.cer
+rm -f *.p12
+rm -f *.pem
+rm -f *.crt
+rm -f *.csr
 ./createCACerts.sh
 ./createCustomCertReq.sh
 ./signateCustomCert.sh server_req.csr server_cert.crt
--- createCustomCertReq.sh
+++ createCustomCertReq.sh
@@ -1,6 +1,6 @@
 ## http://rhythm-zju.blog.163.com/blog/static/310042008015115718637/
 ## http://www.360doc.com/content/15/1224/18/18635950_522843628.shtml
-openssl genrsa -des3 -out server_key.key.orig
+openssl genrsa -des3 -days 3650 -out server_key.key.orig
 openssl rsa -in server_key.key.orig -out server_key.key
 rm -f server_key.key.orig
 openssl req -new -days 3650 -key server_key.key -out server_req.csr
